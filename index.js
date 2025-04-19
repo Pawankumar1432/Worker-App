a303f1426dd371a468f14b48127937e330d769ed
Pavankumar <byallpawan@gmail.com>
2025-04-19 12:29:15 +0530

    first :commit

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-document.querySelectorAll('.service-box').forEach(box => {
-    box.addEventListener('click', () => {
-      alert(`${box.innerText} service selected!`);
-      // You can redirect to other pages or trigger modals here
-      
-    });
-  });
-  
+ 
